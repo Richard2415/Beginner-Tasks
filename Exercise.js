45e9d6d9a1492ed79d5f17ed4b90ac8fdc8f625f
Richard2415 <richardselvaraj07@gmail.com>
2022-08-03 12:22:09 +0530

    final commit

---
+++ Exercise.js
@@ -0,0 +1,60 @@
+'Task 1:'
+"Define a Start and Stop in console and add a delay between them, to display Stop after five Seconds, the output should be Start after 5secs Stop will print."
+
+'Use Case:'
+"Showing a message to the user after the specified time"
+
+'Real Time Example:'
+"Alert a user after certain amount of time or alert the user that a certain work has completed,So using this we will not wait for the task to be finished in the mean time we will do some other work and we can handle both success and failure('If the work gets completed it will display the result, if it fails it will display the error message')"
+
+'---------------------------------------------------------------------------------'
+
+'Task 2:'
+"Stack Implementation(LIFO)"
+
+'Use Case:'
+"Undo Operation (generally used in editors, drawing app), Stack follows First In Last Out(FILO) principle"
+
+'Real Time Example:'
+"Stack of plates is maintained in hotel, Where Plate at the top will get removed first"
+"People are entering an elevator,the one who enters last gets a chance to get out first"
+
+'---------------------------------------------------------------------------------'
+
+'Task 3:'
+"Queue Implementation(FIFO)"
+
+'Use Case:'
+"Nowadays computer handles multiuser, multiprogramming environment and time-sharing environment. In this environment a computer handles several jobs at a time, to handle these jobs the concept of a queue is used, Queue follows First In First Out(FIFO) principle"
+
+'Real Time Example:'
+"In Whatsapp When we send messages to our friends and they don't have internet connection then these messages are queued on the server of Whatsapp."
+
+'---------------------------------------------------------------------------------'
+
+
+'Task 4:'
+"Bus Ticket Fare Calculator"
+
+'Use Case:'
+"A pre-programmed ticket Electronic Ticket Machines(ETM) with computer processor and memory"
+
+'Real Time Example:'
+"Now a days Bus Conductor uses Electronic Ticket Machine to issue ticket to the passengers, by entering(button) a destination stop it will generates a ticket with the price for that stop."
+
+'---------------------------------------------------------------------------------'
+
+
+'Task 5:'
+"Implementing Binary Search Tree"
+
+'Use Case:'
+"A Tree is a hierarchical data Structure that consists of Nodes connected by Edges"
+"Each node stores a datat value"
+"A tree is a non linear data structure, compared to arrays, linked lists,stacks and queues which are linear data structures"
+"In linear data structures, the time required to search is proportional to the size of the data set(more the data, more the time needed to search)"
+"Trees follows nonlinear nature allows quicker and easier access to the data"
+
+'Real Time Example:'
+"Consider a list of 100 numbers, in ascending order. We need to find number 78"
+"Suppose We know the root node(consider as 50) than compare 78 with 50 if it is greater go Right hand side(RHS), if it is less than 50 go Left hand side(LHS), in this way binary tree search is more efficient."
