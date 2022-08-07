840f43eec0ecbe32dbecc8cff8850407f5ed3681
Richard2415 <richardselvaraj07@gmail.com>
2022-08-07 18:51:32 +0530

    final commit

---
+++ maxCharacter.js
@@ -0,0 +1,24 @@
+//Method 1:
+const maxChar = (str) => {
+    let obj = {};
+    for (let char of str)
+        (!obj[char]) ? obj[char] = 1 : obj[char]++;
+    console.log(obj);
+}
+
+maxChar('richardselvaraj');
+
+//Method 2:
+function maxCharacter(str) {
+    const charMap = {};
+    str.split('').forEach((char) => {
+        if (charMap[char]) {
+            charMap[char]++;
+        } else {
+            charMap[char] = 1;
+        }
+    })
+    console.log(charMap);
+}
+
+maxCharacter('richardselvaraj');
